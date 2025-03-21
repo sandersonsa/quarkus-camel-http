2a88cd416f84c1c9b4b2a0eaaffa96acfa00b825
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2025-03-21 16:25:11 -0300

    add cookies 2

--- src/main/java/xyz/sandersonsa/route/CamelRoute.java
+++ src/main/java/xyz/sandersonsa/route/CamelRoute.java
@@ -1,5 +1,7 @@
 package xyz.sandersonsa.route;
 
+import java.time.LocalDateTime;
+
 import org.apache.camel.builder.RouteBuilder;
 
 public class CamelRoute  extends RouteBuilder {
@@ -7,10 +9,17 @@ public class CamelRoute  extends RouteBuilder {
     @Override
     public void configure() {
 
+        String dateTimeStr = LocalDateTime.now().toString();
+        // System.out.println(dateTimeStr);
+
         from("timer:foo?period={{timer.period}}")
                 .setHeader("User-Agent", header("User-Agent"))
                 .setHeader("Accept", header("Accept"))
-                .to("{{rest.endpoint.url}}")
-                .log("Response=${body}");        
+                // .setHeader("CamelHttpCookie", simple("cookieName=" + LocalDateTime.now().toString()))
+                // .setHeader("Cookie", constant("curio="+ System.currentTimeMillis()))
+                .process(exchange -> exchange.getIn().setHeader("Cookie", "curio=" + System.currentTimeMillis()))
+                .toD("{{rest.endpoint.url}}")
+                .log("Response=${body}")
+                .log(dateTimeStr);        
     }
 }
